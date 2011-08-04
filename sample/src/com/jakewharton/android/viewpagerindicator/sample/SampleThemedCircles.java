64e95fef25c615aa057b0dd941c36955e6ac9ac5
Jake Wharton <jakewharton@gmail.com>
2011-08-04 15:47:42 -0400

    Add themed samples.

---
+++ sample/src/com/jakewharton/android/viewpagerindicator/sample/SampleThemedCircles.java
@@ -0,0 +1,20 @@
+package com.jakewharton.android.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import com.jakewharton.android.viewpagerindicator.CirclePageIndicator;
+
+public class SampleThemedCircles extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.themed_circles);
+		
+		ViewPager pager = (ViewPager)findViewById(R.id.pager);
+		pager.setAdapter(new TestFragmentAdapter(getSupportFragmentManager()));
+		
+		CirclePageIndicator indicator = (CirclePageIndicator)findViewById(R.id.indicator);
+		indicator.setViewPager(pager);
+	}
+}
