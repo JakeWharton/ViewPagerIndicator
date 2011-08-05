a0fe8b2355b47a52d305e3a03f707f1e23cd34e3
Jake Wharton <jakewharton@gmail.com>
2011-08-05 12:23:59 -0400

    Add snap sample.

---
+++ sample/src/com/jakewharton/android/viewpagerindicator/sample/SampleCirclesSnap.java
@@ -0,0 +1,21 @@
+package com.jakewharton.android.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import com.jakewharton.android.viewpagerindicator.CirclePageIndicator;
+
+public class SampleCirclesSnap extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.simple_circles);
+		
+		ViewPager pager = (ViewPager)findViewById(R.id.pager);
+		pager.setAdapter(new TestFragmentAdapter(getSupportFragmentManager()));
+		
+		CirclePageIndicator indicator = (CirclePageIndicator)findViewById(R.id.indicator);
+		indicator.setViewPager(pager);
+		indicator.setSnap(true);
+	}
+}
