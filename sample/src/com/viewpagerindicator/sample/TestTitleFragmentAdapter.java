f7b793cc20b2022969e1b17ffb3a4bb5f5bb665b
Gabriel Ittner <gabriel.ittner@googlemail.com>
2012-03-05 14:57:53 +0100

    applied changes to samples

--- sample/src/com/viewpagerindicator/sample/TestTitleFragmentAdapter.java
+++ sample/src/com/viewpagerindicator/sample/TestTitleFragmentAdapter.java
@@ -1,15 +1,14 @@
 package com.viewpagerindicator.sample;
 
 import android.support.v4.app.FragmentManager;
-import com.viewpagerindicator.TitleProvider;
 
-class TestTitleFragmentAdapter extends TestFragmentAdapter implements TitleProvider {
+class TestTitleFragmentAdapter extends TestFragmentAdapter {
 	public TestTitleFragmentAdapter(FragmentManager fm) {
 		super(fm);
 	}
 
 	@Override
-	public String getTitle(int position) {
+	public CharSequence getPageTitle(int position) {
 		return TestFragmentAdapter.CONTENT[position % CONTENT.length];
 	}
 }
