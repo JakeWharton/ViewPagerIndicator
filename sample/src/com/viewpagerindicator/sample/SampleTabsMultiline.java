bb021ed688e69236e184d808283ad197743f80f4
Kiran Rao <techie.curious@gmail.com>
2013-01-11 21:05:33 -0800

    Sample for Multiline tabs.
    This new sample shows how to create tab indicators that take up more than one line.

---
+++ sample/src/com/viewpagerindicator/sample/SampleTabsMultiline.java
@@ -0,0 +1,57 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.Random;
+
+import com.viewpagerindicator.TabPageIndicator;
+
+public class SampleTabsMultiline extends FragmentActivity {
+    private static final String[] CONTENT = new String[] { "Recent", "Artists", "Albums", "Songs", "Playlists", "Genres" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_tabs);
+
+        FragmentPagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
+
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+    }
+
+    class GoogleMusicAdapter extends FragmentPagerAdapter {
+        private final Random RANDOM = new Random();
+        public GoogleMusicAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return new StringBuilder(CONTENT[position % CONTENT.length].toUpperCase())
+                    .append("\n")
+                    .append("(")
+                    .append(RANDOM.nextInt(1000))
+                    .append(")")
+                    .toString();
+        }
+
+        @Override
+        public int getCount() {
+            return CONTENT.length;
+        }
+    }
+}
