53acd6f6b7936aac550258f3239411e2497aee38
bartwell <web@bartwell.ru>
2013-07-25 20:09:04 +0400

    Added new IndicatorStyle - Block

---
+++ sample/src/com/viewpagerindicator/sample/SampleTitlesBlock.java
@@ -0,0 +1,30 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.viewpagerindicator.TitlePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator.IndicatorStyle;
+
+public class SampleTitlesBlock extends BaseSampleActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_titles);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        indicator.setFooterIndicatorStyle(IndicatorStyle.Block);
+        indicator.setFooterLineHeight(0);
+        indicator.setBackgroundColor(0xffaaaaaa);
+		indicator.setTextColor(0xff000000);
+		indicator.setSelectedColor(0xffffffff);
+        indicator.setFooterColor(0xff666666);
+        mIndicator = indicator;
+    }
+}
