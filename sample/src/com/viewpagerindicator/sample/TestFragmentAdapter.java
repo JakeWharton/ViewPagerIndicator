04b9c59812a220bb89dff67e92472ebb92b59dde
Emil Atanasov <e.atanasov@appcom-interactive.de>
2014-05-06 16:46:40 +0200

    Make the icons scrollable. The gravity property is not needed when there are to many icons. Compensate that by putting the wievpager indicator in the center.

--- sample/src/com/viewpagerindicator/sample/TestFragmentAdapter.java
+++ sample/src/com/viewpagerindicator/sample/TestFragmentAdapter.java
@@ -14,7 +14,7 @@ class TestFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapt
             R.drawable.perm_group_location
     };
 
-    private int mCount = CONTENT.length;
+    private int mCount = 5 * CONTENT.length;
 
     public TestFragmentAdapter(FragmentManager fm) {
         super(fm);
@@ -41,7 +41,7 @@ class TestFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapt
     }
 
     public void setCount(int count) {
-        if (count > 0 && count <= 10) {
+        if (count > 0 && count <= 20) {
             mCount = count;
             notifyDataSetChanged();
         }
