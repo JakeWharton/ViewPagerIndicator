7b2ad6bfcd06ca6e93db7c06a7a83178cb42bc2f
Clement BOULY <bouly.clement@gmail.com>
2014-07-21 14:45:51 +0200

    Gradle implementation

---
+++ build/generated/source/buildConfig/release/com/viewpagerindicator/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.viewpagerindicator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.viewpagerindicator";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 65;
+  public static final String VERSION_NAME = "";
+}
