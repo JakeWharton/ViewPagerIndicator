e1a639933b97b6f979382326e243e820c2d3bddb
msdx <645079761@qq.com>
2017-01-05 18:57:57 +0800

    Use Math instead of deprecated FloatMath

--- library/src/main/java/com/viewpagerindicator/LinePageIndicator.java
+++ library/src/main/java/com/viewpagerindicator/LinePageIndicator.java
@@ -27,7 +27,6 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewConfigurationCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.FloatMath;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -369,7 +368,7 @@ public class LinePageIndicator extends View implements PageIndicator {
                 result = Math.min(result, specSize);
             }
         }
-        return (int)FloatMath.ceil(result);
+        return (int)Math.ceil(result);
     }
 
     /**
@@ -395,7 +394,7 @@ public class LinePageIndicator extends View implements PageIndicator {
                 result = Math.min(result, specSize);
             }
         }
-        return (int)FloatMath.ceil(result);
+        return (int)Math.ceil(result);
     }
 
     @Override
